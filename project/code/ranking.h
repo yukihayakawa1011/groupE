2792c5d0b9d23821ed8633e213fa3e72266dffb5
Sakuma Yuuka <221040@st.yoshida-g.ac.jp>
2023-12-05 09:39:02 +0900

    リザルトからスコアを持ってきて反映する処理

--- project/code/ranking.h
+++ project/code/ranking.h
@@ -14,7 +14,8 @@
 //===============================================
 #define NUM_RANK	(3)		// ランキングの順位数
 #define NUM_RANKING (2)     // 表示するランキングの数
-#define MAX_RANKING (6)
+#define MAX_RANKING (4)
+#define NUM_NOWSCORE (2)	//スコア
 
 // 前方宣言
 class CScore;
@@ -41,6 +42,7 @@ public:
 	void Draw(void);
 
 	static void SetScore(int nValue) { m_nScore = nValue; }
+	static void SetTotalScore(int nValue) { m_nTotalScore = nValue; }
 
 private:
 
@@ -53,7 +55,9 @@ private:
 	// メンバ変数
 	CFileLoad *m_pFileLoad;	// ファイル読み込みのポインタ
 	static CScore *m_apScore[NUM_RANKING][NUM_RANK];	// スコアのポインタ(順位分)
+	static CScore *m_apNowScore[NUM_NOWSCORE];	// スコアのポインタ(個人、チーム)
 	static int m_nScore;	// 今回のスコア
+	static int m_nTotalScore;	// 今回のスコア
 	int m_nTimer;			// 自動遷移タイマー
 	int m_nRank;
 	CCarManager *m_pCarManager;		// カーマネージャーのポインタ
