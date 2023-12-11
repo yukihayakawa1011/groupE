ec126a8da697a16ab9eb71051ff72692c776e2ba
ryosukeohara <221109@st.yoshida-g.ac.jp>
2023-12-11 13:07:08 +0900

    スタート時のノルマのUI

--- project/code/quataui.cpp
+++ project/code/quataui.cpp
@@ -12,12 +12,11 @@
 
 // 無名名前空間
 namespace {
-	const int DEF_PRI = (7);	// 優先順位
+	const int DEF_PRI = (7);                        // 優先順位
+	const D3DXVECTOR2 MOVESPEED = { 8.0f, 5.0f };	// 動く速さ
 	const char* FILENAME[CQuataUI::TYPE_MAX] = {	// テクスチャファイル名
-		"data\\TEXTURE\\quata000.png",
-		"data\\TEXTURE\\quata001.png",
-		"data\\TEXTURE\\quata002.png",
-
+		"data\\TEXTURE\\quataui000.png",
+		"data\\TEXTURE\\quataui001.png",
 	};
 }
 
@@ -27,9 +26,13 @@ namespace {
 CQuataUI::CQuataUI()
 {
 	// 値のクリア
-	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
-	m_nLife = 0;
+	m_Info.m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_Info.m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_Info.m_col = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	m_Info.m_state = STATE_NONE;
+	m_Info.m_type = TYPE_START;
+	m_Info.m_fHeight = 0.0f;
+	m_Info.m_fWidht = 0.0f;
 	m_nNumPlayer = 0;
 }
 
@@ -49,7 +52,6 @@ HRESULT CQuataUI::Init(void)
 	if (m_pObject == nullptr)
 	{// 使用されていない場合
 
-
 	}
 
 	return S_OK;
@@ -64,9 +66,9 @@ HRESULT CQuataUI::Init(float fWidth, float fHeight)
 	{// 使用されていない場合
 
 		m_pObject = CObject2D::Create(7);
-		m_pObject->BindTexture(CManager::GetInstance()->GetTexture()->Regist(FILENAME[m_type]));
-		m_pObject->SetPosition(m_pos);
-		m_pObject->SetSize(fWidth, fHeight);
+		m_pObject->BindTexture(CManager::GetInstance()->GetTexture()->Regist(FILENAME[m_Info.m_type]));
+		m_pObject->SetPosition(m_Info.m_pos);
+		m_pObject->SetSize(m_Info.m_fWidht, m_Info.m_fHeight);
 	}
 
 	return S_OK;
@@ -91,13 +93,83 @@ void CQuataUI::Uninit(void)
 //==========================================================
 void CQuataUI::Update(void)
 {
-	
+	switch (m_Info.m_state)
+	{
+	case STATE_NONE:
+		break;
+
+	case STATE_MOVE:
+
+		m_Info.m_pos.x -= MOVESPEED.x;
+
+		m_pObject->SetPosition(m_Info.m_pos);
+
+		if (m_Info.m_pos.x <= SCREEN_WIDTH * 0.5f)
+		{
+			m_Info.m_state = STATE_SCALING;
+		}
+
+		break;
+
+	case STATE_SCALING:
+
+		float fDestWidth, fDestHeight;
+
+		fDestWidth = 100.0f - (m_Info.m_fWidht * 0.5f);
+
+		fDestHeight = 50.0f - (m_Info.m_fHeight * 0.5f);
+
+		m_Info.m_fWidht += fDestWidth;
+
+		m_Info.m_fHeight += fDestHeight;
+
+		if (m_Info.m_fHeight >= 100.0f && m_Info.m_fWidht >= 200.0f)
+		{
+			m_Info.m_state = STATE_UP;
+		}
+		break;
+
+	case STATE_UP:
+
+		m_Info.m_pos.y -= MOVESPEED.y;
+		m_Info.m_fWidht -= 0.9f;
+		m_Info.m_fHeight -= 0.7f;
+		m_pObject->SetPosition(m_Info.m_pos);
+
+		if (m_Info.m_pos.y <= SCREEN_HEIGHT * 0.2f)
+		{
+			m_Info.m_state = STATE_LOST;
+		}
+		break;
+
+	case STATE_LOST:
+
+		m_Info.m_col.a -= 0.05f;
+
+		m_pObject->SetCol(m_Info.m_col);
+
+		if (m_Info.m_col.a <= 0.5f)
+		{
+			m_Info.m_state = STATE_NONE;
+		}
+
+		break;
+
+	case STATE_MAX:
+		break;
+
+	default:
+		break;
+	}
+
+	// サイズ設定
+	m_pObject->SetSize(m_Info.m_fWidht, m_Info.m_fHeight);
 }
 
 //==========================================================
 // 生成
 //==========================================================
-CQuataUI *CQuataUI::Create(D3DXVECTOR3 pos, QUATATYPE type, float fWidth, float fHeight)
+CQuataUI *CQuataUI::Create(D3DXVECTOR3 pos, QUATATYPE type, STATE state, float fWidth, float fHeight)
 {
 	CQuataUI *pUI = nullptr;
 
@@ -105,10 +177,12 @@ CQuataUI *CQuataUI::Create(D3DXVECTOR3 pos, QUATATYPE type, float fWidth, float 
 
 	if (pUI != nullptr)
 	{
-		pUI->SetPosition(pos);
-		pUI->SetType(type);
-		// 初期化処理
-		pUI->Init(fWidth, fHeight);
+		pUI->SetPosition(pos);         // 位置設定
+		pUI->SetWight(fWidth);         // 横幅設定
+		pUI->SetHeight(fHeight);       // 高さ設定
+		pUI->SetType(type);            // 種類設定
+		pUI->SetState(state);          // 状態設定
+		pUI->Init(fWidth, fHeight);    // 初期化処理
 	}
 
 	return pUI;
