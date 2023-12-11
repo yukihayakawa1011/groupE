8b78c851ee7a1398af6c2ab3cd0de1c164e87187
Soma Ishihara <221008@st.yoshida-g.ac.jp>
2023-12-11 11:57:50 +0900

    スコアの数字のdeleteし忘れ修正

--- project/code/game.cpp
+++ project/code/game.cpp
@@ -487,16 +487,17 @@ void CGame::Uninit(void)
 		}
 	}
 
+	if (m_QuataScore != nullptr)
+	{
+		m_QuataScore->Uninit();
+		delete m_QuataScore;
+		m_QuataScore = nullptr;
+	}
+
 	if (m_pPause != nullptr) {
 		m_pPause->Uninit();
 		delete m_pPause;
 		m_pPause = nullptr;
-	}
-
-	if (m_pMiniMap != nullptr)
-	{
-		/*m_pMiniMap->Uninit();
-		m_pMiniMap = nullptr;*/
 	}
 
 	if (m_pFileLoad != nullptr)
