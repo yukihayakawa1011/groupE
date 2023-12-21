de332010169a74ac65aabffce5f207c0b65b54f7
Soma Ishihara <221008@st.yoshida-g.ac.jp>
2023-12-21 11:37:06 +0900

    脱出後の表示がされないのを修正

--- project/code/player.cpp
+++ project/code/player.cpp
@@ -2923,6 +2923,7 @@ void CPlayer::GoalWait(void)
 				col.a += 0.01f;
 			}
 			m_pFade->SetCol(col);
+			m_pFade->SetCol(true);
 		}
 
 		// フェードの色を濃くする
@@ -2932,6 +2933,7 @@ void CPlayer::GoalWait(void)
 				col.a += 0.01f;
 			}
 			m_pEscape->SetCol(col);
+			m_pEscape->SetCol(true);
 		}
 		return;
 	}
